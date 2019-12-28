17f6466fb086867a3672b5b16b51aa07fe9aa27f
huchaojun <huchaojun@izunyou.com>
2019-12-28 19:06:35 +0800

    2.1 计数器应用示例

--- yimu/lib/page/home/two/ks_counter_application_page_2.1.dart
+++ yimu/lib/page/home/two/ks_counter_application_page_2.1.dart
@@ -1,24 +1,47 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:yimu/module/base/ks_stateful_widget.dart';
 import 'package:yimu/module/category/ks_appbar_category.dart';
 
 class KSCounterPpplicationPage extends KSStatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _KSCounterPpplicationPageState();
   }
-
 }
 
 class _KSCounterPpplicationPageState extends State<KSCounterPpplicationPage> {
+  int _counter = 0;
+
+  _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: KSAppBar.ks_init(title: widget.pageTitle),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //start:垂直顶部对齐，center:垂直中心对齐，end：垂直底部对齐
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
-
 }
